736ffc966cb2e928307a9d776b238a741f13f625
Jean <jean.soro007@gmail.com>
2019-12-16 17:44:58 -0500

    Query and display db data into template

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -4,11 +4,32 @@ const SqlString = require('sqlstring')
 
 class ProductController {
 
-  index({
-    view
+  async index({
+    view,
+    request,
+    response
   }) {
-    return view.render('admin/products/all')
+    try {
+      let allProducts = await Database.raw(`
+
+      SELECT * FROM products
+      
+      `)
+      allProducts = allProducts[0] //results
+
+      return view.render('admin/products/all', {
+        allProducts
+      })
+
+    } catch (error) {
+      console.log(error)
+      return response.redirect('back')
+      // return `<h1 style="color: red">Sorry, there was an error</h1>
+      // <h3>${error.sqlMessage}</h3>`
+    }
+
   }
+
   async store({
     request,
     response
@@ -20,35 +41,48 @@ class ProductController {
       INSERT INTO products (title, sku, material, description, brand_id, qty, size, user_id)
       VALUES(${SqlString.escape(post.title)}, ${SqlString.escape(post.sku)}, ${SqlString.escape(post.material)}, ${SqlString.escape(post.description)}, ${parseInt(1)}, ${SqlString.escape(post.qty)}, ${SqlString.escape(post.size)}, ${parseInt(1)})
       `)
-      return `<h1 style="color: green">Saved successfully</h1>`
+      return response.redirect('/admin/products')
 
     } catch (error) {
       console.log(error)
-      return `<h1 style="color: red">Sorry, there was an error</h1>
-      <h3>${error.sqlMessage}</h3>`
+      return response.redirect('back')
+      // return `<h1 style="color: red">Sorry, there was an error</h1>
+      // <h3>${error.sqlMessage}</h3>`
     }
 
 
   }
   create({
-    view
+    view,
+    request,
+    response
   }) {
     return view.render('admin/products/create')
   }
   show({
-    view
+    view,
+    request,
+    response
   }) {
     return view.render('admin/products/show')
   }
   edit({
-    view
+    view,
+    request,
+    response
   }) {
     return view.render('admin/products/edit')
   }
-  update() {
+  update({
+    request,
+    response
+  }) {
 
   }
-  delete() {
+  delete({
+    request,
+    response
+  }) {
 
   }
 }
