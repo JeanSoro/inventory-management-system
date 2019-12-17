720c2455dd9d9b7ef47dd2672bd746027371ecf2
Jean <jean.soro007@gmail.com>
2019-12-17 17:38:01 -0500

    Set up logic for show produt page

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -74,12 +74,50 @@ class ProductController {
   }) {
     return view.render('admin/products/create')
   }
-  show({
+  async show({
     view,
     request,
-    response
+    response,
+    params
   }) {
-    return view.render('admin/products/show')
+    try {
+      let product = await Database.raw(`
+
+      SELECT products.id,
+        products.title,
+        products.sku,
+        products.img_url,
+        products.description,
+        brands.title as brand,
+        concat(users.f_name, ' ', users.l_name) as user,
+        products.material,
+        products.qty,
+        products.size,
+        products.user_id,
+        products.created_at
+        FROM products
+        INNER JOIN brands
+        ON products.brand_id = brands.id
+        INNER JOIN users
+        ON products.user_id = users.id
+        WHERE products.id = ${params.id}
+        ORDER BY created_at ASC
+        LIMIT 1
+      
+      `)
+
+      product = product[0][0] //results
+
+      return view.render('admin/products/show', {
+        product
+      })
+
+    } catch (error) {
+      console.log(error)
+      return response.redirect('back')
+      // return `<h1 style="color: red">Sorry, there was an error</h1>
+      // <h3>${error.sqlMessage}</h3>`
+    }
   }
   edit({
     view,
