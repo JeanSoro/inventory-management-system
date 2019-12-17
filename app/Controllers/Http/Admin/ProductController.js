8ac1709d47388193d19e0a1c9ac1c2ea78aa8b1b
Jean <jean.soro007@gmail.com>
2019-12-17 10:51:34 -0500

    Inner join 3 tables and render data into template

--- app/Controllers/Http/Admin/ProductController.js
+++ app/Controllers/Http/Admin/ProductController.js
@@ -12,7 +12,22 @@ class ProductController {
     try {
       let allProducts = await Database.raw(`
 
-      SELECT * FROM products
+      SELECT products.id,
+        products.title,
+        products.sku,
+        brands.title as brand,
+        concat(users.f_name, ' ', users.l_name) as user,
+        products.material,
+        products.qty,
+        products.size,
+        products.user_id,
+        products.created_at
+        FROM products
+        INNER JOIN brands
+        ON products.brand_id = brands.id
+        INNER JOIN users
+        ON products.user_id = users.id
+        ORDER BY created_at ASC
       
       `)
       allProducts = allProducts[0] //results
